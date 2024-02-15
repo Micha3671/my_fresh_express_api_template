56f7cedf6bd25ed735b1d2d209d925fbede57f77
Micha <michael.rigas@t-online.de>
2024-02-15 15:19:26 +0100

    main commit

---
+++ src/routes/todos/index.js
@@ -0,0 +1,84 @@
+const { Router } = require("express");
+const { StatusCodes, ReasonPhrases } = require("http-status-codes");
+
+const TodosRouter = Router();
+
+const todos = [
+  {
+    id: 1,
+    userId: 1,
+    task: "Wäsche waschen",
+    isDone: true,
+    dueDate: new Date("2024-03-03"),
+  },
+  {
+    id: 2,
+    userId: 1,
+    task: "Müll rausbrigen",
+    isDone: false,
+    dueDate: new Date("2024-03-03"),
+  },
+  {
+    id: 3,
+    userId: 2,
+    task: "Tanzen",
+    isDone: false,
+    dueDate: new Date("2024-03-03"),
+  },
+  {
+    id: 4,
+    userId: 2,
+    task: "Auto fahren",
+    isDone: true,
+    dueDate: new Date("2024-03-03"),
+  },
+];
+
+// GET REQUESTS
+// /v1/todos/bytodoid
+TodosRouter.get("/byid", (req, res) => {
+  const todoId = req.query.todoId;
+  if (!todoId) {
+    res.status(StatusCodes.BAD_REQUEST).send(ReasonPhrases.BAD_REQUEST);
+    return;
+  }
+  res.status(StatusCodes.OK).send("Get Todo by (todo) id");
+});
+
+// Alle Todos von einer UserId
+TodosRouter.get("/byuserid", (req, res) => {
+  const userId = req.body.userId;
+  //const userId = req.query.userId;
+
+  if (!userId) {
+    res
+      .status(StatusCodes.BAD_REQUEST)
+      .send(ReasonPhrases.BAD_REQUEST + " Keine userID");
+    return;
+  }
+
+  const userTodos = todos.filter((todo) => todo.userId === userId);
+
+  res.status(StatusCodes.OK).json(userTodos);
+  // res.status(StatusCodes.OK).send(JSON.stringify(userTodos)); //alternativ
+});
+
+// PUT REQUESTS
+TodosRouter.put("/mark", (req, res) => {
+  res.status(StatusCodes.OK).send("Todo als erledeigt markieren");
+});
+TodosRouter.put("/update", (req, res) => {
+  res.status(StatusCodes.OK).send("Todo aktuallisieren");
+});
+
+// POST REQUESTS
+TodosRouter.post("/create", (req, res) => {
+  res.status(StatusCodes.OK).send("Erstellen eines Todos");
+});
+
+// DELETE REQUEST
+TodosRouter.delete("/delete", (req, res) => {
+  res.status(StatusCodes.OK).send("DELTE Todo");
+});
+
+module.exports = { TodosRouter };
